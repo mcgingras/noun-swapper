394b1b9a46826a4a2ecea98b756815f359e1a9e9
Michael Gingras <mcg79@cornell.edu>
2024-07-02 09:24:27 -0600

    Adds oz for ownable

--- src/NounSwapper.sol
+++ src/NounSwapper.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract NounSwapper {
-    uint256 public number;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+/// this contract will custody nouns.
+/// the owner of the contract can send the nouns out and do whatever else on the nouns.
+/// everyone else can send in a noun, and get back out a noun that is custodied by this contract.
+contract NounSwapper is Ownable {
+    constructor(address _initialOwner) Ownable(_initialOwner) {
+        // nothing else?
     }
 
-    function increment() public {
-        number++;
+    function swapNoun() public {
+        // do something
     }
 }
