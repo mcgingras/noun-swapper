d7a8e479bb95288d954875e1f3757c8102a57695
Michael Gingras <mcg79@cornell.edu>
2024-07-02 11:11:40 -0600

    Updates interface

--- src/interfaces/IERC721Checkpointable.sol
+++ src/interfaces/IERC721Checkpointable.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 /// @dev Interface for interacting with the Nouns ERC721 governance token with minimal deployment bytecode overhead
 interface IERC721Checkpointable {
-    /// @notice The address owning a given tokenId
     function ownerOf(uint256 tokenId) external view returns (address);
+    function balanceOf(address owner) external view returns (uint256);
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
+    function transferFrom(address from, address to, uint256 tokenId) external;
 }
